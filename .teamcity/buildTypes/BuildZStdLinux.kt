00ef8f08d3c6ef5b6d8e657ddee35a7958fb33a3
Prodo Elmit <prodoelmit@gmail.com>
2025-11-22 16:49:56 +0200

    Use docker for cross-compilers env

--- .teamcity/buildTypes/BuildZStdLinux.kt
+++ .teamcity/buildTypes/BuildZStdLinux.kt
@@ -1,6 +1,7 @@
 package buildTypes
 
 import jetbrains.buildServer.configs.kotlin.BuildType
+import jetbrains.buildServer.configs.kotlin.buildSteps.dockerCommand
 import jetbrains.buildServer.configs.kotlin.buildSteps.script
 import vcsRoots.ZStd
 
@@ -12,29 +13,41 @@ object BuildZStdLinux : BuildType({
         root(ZStd, "+:.")
     }
 
+    val dockerTag =  "ubuntu_with_cross_compilers:local"
+
     steps {
-        script {
-            name = "Install cross-compilers"
-            val deps = listOf(
-                "build-essential",
-                "gcc-aarch64-linux-gnu",
-                "g++-aarch64-linux-gnu",
-            )
-            scriptContent = """
-                apt install -y --no-install-recommends ${deps.joinToString(" ")}
-            """.trimIndent()
+        dockerCommand {
+            name = "Prepare docker with cross-compilers"
+            this.commandType = build {
+                val deps = listOf(
+                    "build-essential",
+                    "gcc-aarch64-linux-gnu",
+                    "g++-aarch64-linux-gnu",
+                )
+                this.source = content {
+                    this.content = """
+                        FROM ubuntu:22
+                        
+                        apt install -y --no-install-recommends \\${deps.joinToString("\\n")}
+                    """.trimIndent()
+                }
+
+                this.namesAndTags = dockerTag
+            }
         }
         script {
             name = "Build x86_64"
             scriptContent = """
                 make
             """.trimIndent()
+            dockerImage = dockerTag
         }
         script {
             name = "Build aarch64"
             scriptContent = """
                 CC=aarch64-linux-gnu-gcc make
             """.trimIndent()
+            dockerImage = dockerTag
         }
     }
 
